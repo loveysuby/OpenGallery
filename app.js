6adea06c68b5df212643f3cf23a42bd2e5c437e5
loveysuby <crad_on25@khu.ac.kr>
2021-01-06 23:45:02 +0900

    node router 구성,npm package 명시

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var bodyparser = require('body-parser');
+var url = require('url');
+require('dotenv').config();
+
+//화면 별 router 연결, 라우터 호출해서 페이지를 불러오는데 사용함.
+var mainRouter = require('./routes') //호출시 index.js 실행 (main.js : title 할당하고 main.html 열어줌)
 
 //디폴트 포트 값 : 8000 
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 23023);
 
-app.get('/', (req, res) => {
-    //res.send('Server is working');
-    res.sendFile(path.join(__dirname, '/main.html'));
-    console.log(app.get('port'), '번 포트 대기 중');
+//ejs (html포맷) 파일을 웹사이트에 view로 띄워주기 위한 view engine 설정.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+//각각의 요청에서 index.js에 등록된 router 호출해서 page를 전환함.
+app.use('/', mainRouter);
+
+//css, image 등 정적 파일을 public에서 불러옴 -> html과 연결함 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/find", function (req, res) {
+    var urlParsed = url.parse(req.url, true);
+    var querystring = urlParsed.query;
+    // 값이 querystring.word 으로 불러와짐 ( 검색 단어) : 제목에서 find만 구현하면됨.
+    res.send(querystring.word);
 });
 
-app.get('/login', (req, res) => {
-    res.send('로그인 페이지 : 로그인 방법 구상 하고 html 및 사이트 가져오기');
-    console.log('로그인 페이지 오픈 시도됨.');
+app.get('/logout', function (req, res) {
+    res.send("Logout success");
 });
+
+
 app.listen(app.get('port'), () => {
     console.log(`Server is running at ${app.get('port')}`);
 });
